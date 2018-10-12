710e2977466ebe06b8d1aac47818c7f33cb47a72
iwanaminami <iwanamishoya@gmail.com>
2018-10-13 02:07:50 +0900

    loadingfix

--- js/loading.js
+++ js/loading.js
@@ -1,14 +1,14 @@
 $(function() {
   var h = $(window).height();
  
-  $('#wrap').css('display','none');
+  $('#container').css('display','none');
   $('#loader-bg ,#loader').height(h).css('display','block');
 });
  
 $(window).on('load', function() { //全ての読み込みが完了したら実行
   $('#loader-bg').delay(2000).fadeOut(800);
   $('#loader').delay(1800).fadeOut(300);
-  $('#wrap').css('display', 'block');
+  $('#container').css('display', 'block');
 });
  
 //10秒たったら強制的にロード画面を非表示
@@ -17,7 +17,7 @@ $(function(){
 });
  
 function stopload(){
-  $('#wrap').css('display','block');
+  $('#container').css('display','block');
   $('#loader-bg').delay(900).fadeOut(800);
   $('#loader').delay(600).fadeOut(300);
 }

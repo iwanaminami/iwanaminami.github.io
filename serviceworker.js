9ef460aa6ba2da9cffc9ad458502c3e564afdc98
iwanaminami <iwanamishoya@gmail.com>
2018-12-15 20:15:28 +0900

    serviceworker

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,2 @@
-if ('serviceWorker' in navigator) {
-    // ServiceWorker を登録
-    navigator.serviceWorker.register('/serviceworker.js',
-    {
-      scope: '/'
-    })
-};
+self.addEventListener('fetch', function(event) {
+});
